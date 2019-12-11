65d3cd8bf0239cac4938f353d4c99e046764442d
gaosh123 <948212712@qq.com>
2019-12-11 11:55:40 +0800

    task10

--- css/task10/task10.js
+++
@@ -1,4 +0,0 @@
-$(":checkbox").click(function(){
-    $(this).attr("checked",true);//设置当前选中checkbox的状态为checked
-    $(this).siblings().attr("checked",false); //设置当前选中的checkbox同级(兄弟级)其他checkbox状态为未选中
-});
